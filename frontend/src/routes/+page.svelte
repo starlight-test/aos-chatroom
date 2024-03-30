82f86c0c3fe2195ff2fc93c5c934d79ed3bedaf5
pratikbhadane24 <pratikbhadane24@gmail.com>
2024-03-30 13:56:52 +0530

    feat!: replace react -> sveltekit☠️

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Arweave from 'arweave';
+    import {results} from "@permaweb/aoconnect";
+
+    const arweave = Arweave.init({});
+
+    const generateWallet = async () => {
+        const wallet = await arweave.wallets.generate();
+        console.log(wallet);
+    }
+    async function getMessages() {
+        let result = await results({
+            process: "qeNpDBVhSdJQwHLK5LYq-rmzifl3UvPJrCoFuo1qx6E",
+            sort: "ASC",
+            limit: 25,
+        });
+        console.log(result);
+    }
+
+</script>
+
+<button on:click={generateWallet}>Generate Wallet</button>
+<button on:click={getMessages}>Get Messages</button>
