e240ce487f87b05270da250c12b26a8aa71dadaa
Rohit Patil <rahulhimesh09@gmail.com>
2024-03-31 08:23:58 +0530

    feat: send messages

--- frontend/src/components/chat-section/ChatWindow.svelte
+++ frontend/src/components/chat-section/ChatWindow.svelte
@@ -3,62 +3,88 @@
     import { ULID } from "./util.js";
     import MsgLog from "./MsgLog.svelte";
     import Arweave from "arweave";
-    import { createDataItemSigner, message } from "@permaweb/aoconnect";
+    import { createDataItemSigner, message, result } from "@permaweb/aoconnect";
     import type { JWKInterface } from "arweave/node/lib/wallet";
 
+    import { browser } from "$app/environment";
+
+    import type { PermissionType } from "arconnect";
+    import type { MouseEventHandler } from "svelte/elements";
     const arweave = Arweave.init({});
 
-    let imageToUpload: File | null = null;
-    let files: FileList;
-    let fileInput: FileList;
-    let wallet: JWKInterface | null = null;
-
-    let now = new Date().toISOString().substring(11, 16);
+    let address: string | undefined;
     let dayName = new Date().toISOString().substring(0, 10);
+    let fileInput: FileList;
+    let files: FileList;
+    let imageToUpload;
     let myUID = ULID().slice(8);
 
     export let msgs: any[];
 
     let myMsg = "";
-    const sendMessage = async () => {
-        const wallet = globalThis.arweaveWallet;
-        if (!wallet) {
-            alert("Please generate a wallet first");
+
+    const permissions: PermissionType[] = [
+        "ACCESS_ADDRESS",
+        "ACCESS_PUBLIC_KEY",
+        "ACCESS_ALL_ADDRESSES",
+        "SIGN_TRANSACTION",
+        "SIGNATURE",
+        "DISPATCH",
+        "ENCRYPT",
+        "DECRYPT",
+    ];
+
+    const PROCESS_ID = "RUmNZFCeayzCxwzRT7Qiut9z1fQtfswsW0nTbEjcTWw";
+    const sendMessage = async (messageData: string) => {
+        if (!browser) {
             return;
         }
-        uploadImageToArweave();
-        console.log(wallet);
-        await wallet.connect([
-            "ACCESS_ADDRESS",
-            "ACCESS_PUBLIC_KEY",
-            "ACCESS_ALL_ADDRESSES",
-            "SIGN_TRANSACTION",
-            "SIGNATURE",
-            "DISPATCH",
-            "ENCRYPT",
-            "DECRYPT",
-        ]);
-        // console.log({
-        //     activeaddr: await wallet.getActiveAddress(),
-        //     alladdrs: await wallet.getAllAddresses(),
-        // });
-        const msgsigner = createDataItemSigner(wallet);
-        
-        await message({
-            process: "RUmNZFCeayzCxwzRT7Qiut9z1fQtfswsW0nTbEjcTWw",
-            data: myMsg,
-            signer: msgsigner,
+        if (!window.arweaveWallet) {
+            alert("Please Install A arweave compatible wallet first!");
+            return;
+        }
+        if (!address) {
+            await window.arweaveWallet.connect(permissions);
+            console.log("Wallet Connected", {
+                activeaddr: await window.arweaveWallet.getActiveAddress(),
+                alladdrs: await window.arweaveWallet.getAllAddresses(),
+            });
+            address = await window.arweaveWallet.getActiveAddress();
+        }
+        const messageId = await message({
+            process: PROCESS_ID,
+            data: messageData,
+            signer: createDataItemSigner(window.arweaveWallet),
+            tags: [
+                {
+                    name: "Action",
+                    value: "Broadcast",
+                },
+                {
+                    name: "sender",
+                    value: address,
+                },
+                {
+                    name: "Time",
+                    value: Date.now().toString(),
+                },
+            ],
         });
+        const msgres = await result({
+            message: messageId,
+            process: PROCESS_ID,
+        });
+        console.log(msgres);
     };
     const uploadImageToArweave = async () => {
-        if(fileInput){
+        if (fileInput) {
             imageToUpload = fileInput.files[0];
             uploadImageToArweave();
         }
         // Read the contents of the file asynchronously
         const fileReader = new FileReader();
         fileReader.onload = async function () {
-            if (!wallet) {
+            if (!window.arweaveWallet) {
                 alert("Please generate a wallet first");
                 return;
             }
@@ -76,11 +102,15 @@
                 {
                     data: buffer, // Convert the file content to a Buffer
                 },
-                wallet,
+                "use_wallet",
             );
+            transaction.addTag("Content-Type", "image/jpeg");
             console.log(transaction);
-            console.log(wallet);
-            const res = await arweave.transactions.sign(transaction, wallet);
+            console.log(window.arweaveWallet);
+            const res = await arweave.transactions.sign(
+                transaction,
+                "use_wallet",
+            );
 
             // const response = await arweave.transactions.post(transaction);
             // console.log(response);
@@ -98,7 +128,6 @@
         // Start reading the file
         fileReader.readAsArrayBuffer(imageToUpload);
 
-        // transaction.addTag('Content-Type', 'text/html');
         // transaction.addTag('key2', 'value2');
         // await arweave.transactions.sign(transaction, wallet);
         // let uploader = await arweave.transactions.getUploader(transaction);
@@ -107,9 +136,7 @@
         //     console.log(`${uploader.pctComplete}% complete, ${uploader.uploadedChunks}/${uploader.totalChunks}`);
         // }
     };
-
-    function onInput(e) {
-        console.log(e);
+    function onInput(e: MouseEvent) {
         let o = {
             name: myUID,
             msg: myMsg,
@@ -119,7 +146,7 @@
             message: o.msg,
             name: myUID,
             isDisplayable: true,
-            strDate: now,
+            strDate: new Date().toISOString().substring(11, 16);,
         };
         msgs = [...msgs, m];
         sendMessage(m.message);
@@ -155,11 +182,12 @@
         bind:files
         bind:this={fileInput}
     />
+    <button on:click={uploadImageToArweave}> Upload Image to Chatroom </button>
     <div
         class="flex rounded-xl bg-gradient-to-tr from-pink-300 to-blue-300 p-0.5 shadow-lg"
     >
         <button
-            on:click={sendMessage}
+            on:click={onInput}
             class="font-bold text-base bg-white px-8 py-2 rounded-xl"
             >Send</button
         >
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,58 +1,130 @@
 <script lang="ts">
   import Arweave from "arweave";
-  import { results, createDataItemSigner, message } from "@permaweb/aoconnect";
+  import {
+    result,
+    results,
+    createDataItemSigner,
+    message,
+    dryrun,
+  } from "@permaweb/aoconnect";
   import ChatWindow from "../components/chat-section/ChatWindow.svelte";
   import { onMount } from "svelte";
-  import type { JWKInterface } from "arweave/node/lib/wallet";
-  const arweave = Arweave.init({});
-
-  let wallet: JWKInterface | null = null;
-  let messageData: string = "";
-  $: messages = [];
-
-  const generateWallet = async () => {
-    wallet = await arweave.wallets.generate();
-    localStorage.setItem("wallet", JSON.stringify(wallet));
-    console.log(wallet);
+  let allMessages: any[] = [];
+  type Result = {
+    cursor: string;
+    node: {
+      Messages: unknown[];
+      Spawns: unknown;
+      Output: {
+        data: string;
+        print: boolean;
+        prompt: string;
+      };
+    };
   };
-
   async function getMessages() {
-    let result = await results({
+    let firstRes = await results({
       process: "RUmNZFCeayzCxwzRT7Qiut9z1fQtfswsW0nTbEjcTWw",
       sort: "DESC",
-      limit: 100,
     });
-    console.log(result);
-    result = result.edges.map((edge) => edge.node);
-    let got_messages = [];
-    result.forEach((item) => {
-      if (item.Messages.length > 0) {
-        if (item) {
-          console.log(item.Messages[0]);
-          let author = "Anonymous";
-          for (let j = 0; j < item.Messages[0].Tags.length; j++) {
-            if (item.Messages[0].Tags[j].name === "Nickname") {
-              author = item.Messages[0].Tags[j].value;
+    console.log({ firstRes });
+    let firstResEdges: Result[] = firstRes.edges;
+    let got_messages: any[] = [];
+    let messageEdges: Result[] | undefined;
+    let next_cursor = "";
+    while (next_cursor !== "END") {
+      if (next_cursor !== "") {
+        const messages = await results({
+          process: "RUmNZFCeayzCxwzRT7Qiut9z1fQtfswsW0nTbEjcTWw",
+          sort: "DESC",
+          from: next_cursor,
+        });
+        console.log({ messages });
+        messageEdges = messages.edges;
+      }
+      if (next_cursor === "") {
+        firstResEdges.forEach((item) => {
+          const Messages = item.node.Messages;
+          if (Messages.length > 0) {
+            if (item) {
+              console.log(Messages[0]);
+              let author = "Anonymous";
+              for (let j = 0; j < Messages[0].Tags.length; j++) {
+                if (Messages[0].Tags[j].name === "Nickname") {
+                  author = Messages[0].Tags[j].value;
+                }
+              }
+              got_messages.push({
+                name: author,
+                message: Messages[0].Data,
+                isDisplayable: true,
+                // "time": new Date(item.Messages[0].Timestamp).toISOString()
+                time: new Date().toISOString(),
+              });
             }
           }
-          got_messages.push({
-            name: author,
-            message: item.Messages[0].Data,
-            isDisplayable: true,
-            // "time": new Date(item.Messages[0].Timestamp).toISOString()
-            time: new Date().toISOString(),
-          });
+          firstRes;
+        });
+        console.log("Cursor :", firstResEdges.slice(-1)[0].cursor);
+        next_cursor = firstResEdges[0].cursor || "END";
+      }
+      if (messageEdges) {
+        messageEdges.forEach((item) => {
+          const Messages = item.node.Messages;
+          if (Messages.length > 0) {
+            if (item) {
+              console.log(Messages[0]);
+              let author = "Anonymous";
+              for (let j = 0; j < Messages[0].Tags.length; j++) {
+                if (Messages[0].Tags[j].name === "Nickname") {
+                  author = Messages[0].Tags[j].value;
+                }
+              }
+              got_messages.push({
+                name: author,
+                message: Messages[0].Data,
+                isDisplayable: true,
+                // "time": new Date(item.Messages[0].Timestamp).toISOString()
+                time: new Date().toISOString(),
+              });
+            }
+          }
+          firstRes;
+        });
+        const candidate_next_cursor = messageEdges?.[0]?.cursor || "END";
+        console.log(candidate_next_cursor);
+        if (next_cursor === candidate_next_cursor) {
+          console.log("same cursor we fucked");
+          next_cursor = "END";
+        } else {
+          next_cursor = candidate_next_cursor;
         }
       }
-    });
-    messages = got_messages;
+    }
+    allMessages = got_messages;
   }
 
   onMount(async () => {
-    let localWallet = localStorage.getItem("wallet");
-    wallet = localWallet ? JSON.parse(localWallet) : null;
+    const msgId = await message({
+      data: "Members",
+      process: "RUmNZFCeayzCxwzRT7Qiut9z1fQtfswsW0nTbEjcTWw",
+      signer: createDataItemSigner(window.arweaveWallet),
+    });
+    console.log({ msgId });
+    const res = await result({
+      message: msgId,
+      process: "RUmNZFCeayzCxwzRT7Qiut9z1fQtfswsW0nTbEjcTWw",
+    });
+    console.log({ res });
+
+    const dryrunres = await dryrun({
+      process: "RUmNZFCeayzCxwzRT7Qiut9z1fQtfswsW0nTbEjcTWw",
+      data: "Members",
+    });
+    console.log(dryrunres);
     await getMessages();
+    console.log(allMessages);
   });
 </script>
 
-<ChatWindow msgs={messages} />
+<ChatWindow msgs={allMessages} />
